6f7ec841e20d6dcbe6c31f4d4fe4204e19e38f4e
wang <754799607@qq.com>
2022-11-04 18:44:52 +0800

    first2

--- Isosceles2.py
+++ Isosceles2.py
@@ -1,7 +1,13 @@
 num=int(input('输入层数：'))
-for i in range(1,num+1):
+for i in range(1,num):
     for a in range(num-i):
         print('  ',end='')
     for b in range(2*i-1):
         print('*',end=' ')
     print()
+for i in range(num):
+    for j in range(i,2*num-1-i):#i.max=a-1;2*a-1-i.mix=a+a-1-a-(a-1)=a
+        print('*',end=' ')
+    print()
+    for k in range(0,1+i):
+        print('  ',end='')
