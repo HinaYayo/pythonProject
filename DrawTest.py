2397420b7230b5aacce0c757928a2e666f3fec94
wang <you@example.com>
2022-11-05 12:51:55 +0800

    圆

--- DrawTest.py
+++ DrawTest.py
@@ -1,10 +1,17 @@
 import turtle
 import random
 t=turtle.Pen()
-x=random.randrange(-turtle.window_width()//2,turtle.window_width()//2)
-y=random.randrange(-turtle.window_height()//2,turtle.window_height()//2)
-t.penup()
-t.setpos(x, y)
-t.pendown()
+Colors=['red','green','blue','black']
+for i in range(2):
+    x=random.randrange(-turtle.window_width()//4,turtle.window_width()//4)
+    y=random.randrange(-turtle.window_height()//4,turtle.window_height()//4)
+    t.penup()
+    t.setpos(x, y)
+    t.pendown()
+    for a in range(100):
+        num = random.randint(0, 3)
+        t.pencolor(Colors[num])
+        t.circle(50)
+        t.left(88)
 turtle.done()
 
