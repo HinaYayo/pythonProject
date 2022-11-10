765f8b44d3e6abd1da8ff8cf2b9b82fa85edc052
wang <you@example.com>
2022-11-10 10:22:47 +0800

    List

---
+++ List.py
@@ -0,0 +1,17 @@
+commuting = ['car', 'bicycle', 'motorcycle']
+brands = ['Benz', 'GIANT', 'Honda']
+guests = ["Tom", "John", "Mike", "Padma"]
+for name in guests:
+    print(name)
+for name in guests:
+    print(name + '，Good moning!')
+for num in range(0, 3):
+    print('I would like to own a ' + brands[num] + ' ' + commuting[num])
+for guest in guests:
+    print(guest + ", 我可以邀请你共进晚餐吗?")
+print(guests[2] + "不能一起吃饭!" + "\n")
+guests[2] = 'Bob'
+for guest in guests:
+    print(guest + ", 我可以邀请你共进晚餐吗?")
+
+
